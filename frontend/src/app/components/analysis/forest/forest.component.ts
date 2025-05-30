db8c5ff6226ea56119adf6210a9b15f03e9e70b9
Nawaz Khurshid <nawaz1981@gmail.com>
2025-05-30 15:21:05 +0200

    Fix for closing polygon adding the first point at the end

--- frontend/src/app/components/analysis/forest/forest.component.ts
+++ frontend/src/app/components/analysis/forest/forest.component.ts
@@ -1,55 +1,76 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MapInputComponent } from '../../map-input/map-input.component';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ItInputComponent, ItSelectComponent } from 'design-angular-kit';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-forest',
   standalone: true,
-  imports: [MapInputComponent, ItInputComponent, ItSelectComponent, ReactiveFormsModule, NgIf ],
+  imports: [
+    MapInputComponent,
+    ItInputComponent,
+    ItSelectComponent,
+    ReactiveFormsModule,
+    NgIf,
+  ],
   templateUrl: './forest.component.html',
-  styleUrl: './forest.component.scss'
+  styleUrl: './forest.component.scss',
 })
 export class ForestComponent implements OnInit {
-
   // startYear, endYear, geometry, outputName
   // fixed: input1, input2
 
   @Input()
   formContainer?: FormGroup;
 
-  years: any[] = [...Array(10).keys()].map(i => ({value: "" + (new Date().getFullYear() - i), text: "" + (new Date().getFullYear() - i)})).concat([
-    {value: '', text: 'Seleziona anno'}
-  ]);
-
+  years: any[] = [...Array(10).keys()]
+    .map((i) => ({
+      value: '' + (new Date().getFullYear() - i),
+      text: '' + (new Date().getFullYear() - i),
+    }))
+    .concat([{ value: '', text: 'Seleziona anno' }]);
 
   constructor(private _fb: FormBuilder) {}
 
   ngOnInit(): void {
     if (this.formContainer) {
-      this.formContainer.addControl('parameters', this._fb.group({
-        startYear: ['', Validators.required],
-        endYear: ['', Validators.required],
-        outputName: ['', Validators.required],
-        geometry: ['', Validators.required],
-      }));
+      this.formContainer.addControl(
+        'parameters',
+        this._fb.group({
+          startYear: ['', Validators.required],
+          endYear: ['', Validators.required],
+          outputName: ['', Validators.required],
+          geometry: ['', Validators.required],
+        }),
+      );
     }
   }
-  
+
   getPolyline(event: any): void {
-     if (event.type === 'draw:created') {
+    if (event.type === 'draw:created') {
       let polyline = event.layer.getLatLngs()[0];
       console.log('Polyline created:', polyline);
-      this.formContainer?.patchValue({"parameters": {"geometry": this.toGeometryString(polyline)}});
+      this.formContainer?.patchValue({
+        parameters: { geometry: this.toGeometryString(polyline) },
+      });
       this.formContainer?.updateValueAndValidity();
     } else if (event.type === 'draw:deleted') {
-      this.formContainer?.patchValue({"parameters": {"geometry": null}});
+      this.formContainer?.patchValue({ parameters: { geometry: null } });
       this.formContainer?.updateValueAndValidity();
-    } 
+    }
   }
   toGeometryString(polyline: any) {
-    return 'POLYGON((' + polyline.map((point: any) => `${point.lng} ${point.lat}`).join(', ') + '))';
+    polyline.push(polyline[0]); // Close the polygon by adding the first point at the end
+    return (
+      'POLYGON((' +
+      polyline.map((point: any) => `${point.lng} ${point.lat}`).join(', ') +
+      '))'
+    );
   }
-
 }
