e9cca231a5a0dd6cf5fceef1f1d8e9af882c90f3
Nawaz Khurshid <nawaz1981@gmail.com>
2025-06-05 12:45:55 +0200

    Update flood-details.component.ts

--- frontend/src/app/components/analysis/details/flood-details/flood-details.component.ts
+++ frontend/src/app/components/analysis/details/flood-details/flood-details.component.ts
@@ -1,29 +1,38 @@
 import { Component, Input } from '@angular/core';
-import { Elaboration, ElaborationService } from '../../../../elaboration.service';
-import { DatePipe, NgIf } from '@angular/common';
+import {
+  Elaboration,
+  ElaborationService,
+} from '../../../../elaboration.service';
+import { DatePipe, NgForOf, NgIf } from '@angular/common';
 import { MapVisComponent } from '../../../map-vis/map-vis.component';
 
 @Component({
   selector: 'app-flood-details',
   standalone: true,
-  imports: [NgIf, MapVisComponent, DatePipe],
+  imports: [NgIf, MapVisComponent, DatePipe, NgForOf],
   templateUrl: './flood-details.component.html',
-  styleUrl: './flood-details.component.scss'
+  styleUrl: './flood-details.component.scss',
 })
 export class FloodDetailsComponent {
+  @Input()
+  elaboration?: Elaboration;
 
-    @Input()
-    elaboration?: Elaboration; 
-  
-    links?: any[];
-  
-    constructor(private elaborationService: ElaborationService) { }
-    ngOnInit(): void {
-      if (this.elaboration?.status == 'COMPLETED') {
-        this.elaborationService.generatePresignedUrl(this.elaboration.id!, this.elaboration.parameters!.outputName!).subscribe(value => {
-        this.links = Object.keys(value).map(k => ({name: k, value: value[k]}));
+  links?: any[];
+
+  constructor(private elaborationService: ElaborationService) {}
+  ngOnInit(): void {
+    if (this.elaboration?.status == 'COMPLETED') {
+      this.elaborationService
+        .generatePresignedUrl(
+          this.elaboration.id!,
+          this.elaboration.parameters!.outputName!,
+        )
+        .subscribe((value) => {
+          this.links = Object.keys(value).map((k) => ({
+            name: k,
+            value: value[k],
+          }));
         });
-  
-      }
     }
+  }
 }
