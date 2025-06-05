d2df950d85fd0bc314c1bad49b9bec941661b5e2
kazhamiakin <kazhamiakin@gmail.com>
2025-06-05 19:23:02 +0200

    cleanup

--- src/main/java/it/smartcommunitylabdhlab/rsde/service/ElaborationEntityService.java
+++ src/main/java/it/smartcommunitylabdhlab/rsde/service/ElaborationEntityService.java
@@ -27,34 +27,34 @@ public class ElaborationEntityService {
     @Autowired
     private ElaborationRepository elRepository;
 
-    @PostConstruct
-    public void init() {
-        if (elRepository.findById("1").isPresent()) {
-            ElaborationEntity entity = new ElaborationEntity();
-            entity.setId("1");
-            entity.setName("forest");
-            entity.setLocalName("deforestation-2018-2019");
-            entity.setProject("deforestation");
-            entity.setCoreId("8c76c182ced845a19d3adc15c3618ed4");
-            entity.setCreatedAt(new Date());
-            entity.setUpdatedAt(new Date());
-            entity.setStatus("COMPLETED");
-            entity.setWorkflowId("kfp://deforestation/pipeline_deforestation:679e3b35eb604fbea622427e0a5cfb42");
-            entity.setTaskId("kfp+pipeline://deforestation/72b25d056799453da82cbfb24a0b89ef");
-            entity.setTag("forest");
-            Map<String, String> map = new HashMap<>();
-            map.put("startYear", "2018");
-            map.put("endYear", "2019");
-            map.put("input1", "bosco");
-            map.put("geometry", "POLYGON((11.031183173429909 46.088357949131584, 11.031923564323543 46.08843607921081, 11.031708958267417 46.08796357656629, 11.031183173429909 46.088357949131584))");
-            map.put("outputName", "pipeline_forest_output21_19");
-            map.put("input2", "data_s2_deforestation");
+    // @PostConstruct
+    // public void init() {
+    //     if (elRepository.findById("1").isPresent()) {
+    //         ElaborationEntity entity = new ElaborationEntity();
+    //         entity.setId("1");
+    //         entity.setName("forest");
+    //         entity.setLocalName("deforestation-2018-2019");
+    //         entity.setProject("deforestation");
+    //         entity.setCoreId("8c76c182ced845a19d3adc15c3618ed4");
+    //         entity.setCreatedAt(new Date());
+    //         entity.setUpdatedAt(new Date());
+    //         entity.setStatus("COMPLETED");
+    //         entity.setWorkflowId("kfp://deforestation/pipeline_deforestation:679e3b35eb604fbea622427e0a5cfb42");
+    //         entity.setTaskId("kfp+pipeline://deforestation/72b25d056799453da82cbfb24a0b89ef");
+    //         entity.setTag("forest");
+    //         Map<String, String> map = new HashMap<>();
+    //         map.put("startYear", "2018");
+    //         map.put("endYear", "2019");
+    //         map.put("input1", "bosco");
+    //         map.put("geometry", "POLYGON((11.031183173429909 46.088357949131584, 11.031923564323543 46.08843607921081, 11.031708958267417 46.08796357656629, 11.031183173429909 46.088357949131584))");
+    //         map.put("outputName", "pipeline_forest_output21_19");
+    //         map.put("input2", "data_s2_deforestation");
 
-            entity.setParameters(map);
+    //         entity.setParameters(map);
             
-            elRepository.save(entity);            
-        }
-    }
+    //         elRepository.save(entity);            
+    //     }
+    // }
 
     public ElaborationEntity save(ElaborationEntity obj) {
 	    return elRepository.save(obj);
