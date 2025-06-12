fe7bcf1168858b95073332d8e0fe1270681506b4
Nawaz Khurshid <nawaz1981@gmail.com>
2025-06-12 14:53:24 +0200

    Fix for land details

--- frontend/src/app/components/analysis/details/land-details/land-details.component.ts
+++ frontend/src/app/components/analysis/details/land-details/land-details.component.ts
@@ -1,16 +1,38 @@
 import { Component, Input } from '@angular/core';
-import { Elaboration } from '../../../../elaboration.service';
+import {
+  Elaboration,
+  ElaborationService,
+} from '../../../../elaboration.service';
+import { MapVisComponent } from '../../../map-vis/map-vis.component';
+import { NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-land-details',
   standalone: true,
-  imports: [],
+  imports: [MapVisComponent, NgIf, NgForOf],
   templateUrl: './land-details.component.html',
-  styleUrl: './land-details.component.scss'
+  styleUrl: './land-details.component.scss',
 })
 export class LandDetailsComponent {
+  @Input()
+  elaboration?: Elaboration; // Define the type based on your Elaboration model, e.g., Elaboration
 
-    @Input()
-    elaboration?: Elaboration; // Define the type based on your Elaboration model, e.g., Elaboration
-  
+  links?: any[];
+
+  constructor(private elaborationService: ElaborationService) {}
+  ngOnInit(): void {
+    if (this.elaboration?.status == 'COMPLETED') {
+      this.elaborationService
+        .generatePresignedUrl(
+          this.elaboration.id!,
+          this.elaboration.parameters!.outputName!,
+        )
+        .subscribe((value) => {
+          this.links = Object.keys(value).map((k) => ({
+            name: k,
+            value: value[k],
+          }));
+        });
+    }
+  }
 }
